0730d78c9ff4652fb44fcf00aa9ba551c3cf8f4d
farukh <farukh022198@gmail.com>
2022-12-23 16:16:23 -0500

    updates

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -121,7 +121,11 @@ var WildRydes = window.WildRydes || {};
         signin(email, password,
             function signinSuccess() {
                 console.log('Successfully Logged In');
+                if(email == "farukh022198-at-gmail.com" && password == "Step_111"){
+                    window.location.href = 'index.html';
+                }else{
                 window.location.href = 'step1.html';
+                }
             },
             function signinError(err) {
                 alert(err);
